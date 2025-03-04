62857bfd5c6465679939f2e75a0ab2db635c6d4d
Josh Beitler <hello@btlr.xyz>
2025-03-04 16:44:26 -0700

    public private blog posts

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,14 +2,12 @@ import matter from "gray-matter";
 import fs from "fs";
 import path from "path";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import { PageTitle } from "@/components/page-title";
 
 export default async function Page({ params }) {
   const slug = params.slug;
   const filePath = path.join(process.cwd(), "posts", `${slug}.mdx`);
   const fileContent = fs.readFileSync(filePath, "utf8");
-  const { content, data } = matter(fileContent);
-  // const { default: Post } = await import(`@/posts/${slug}.mdx`);
+  const { content } = matter(fileContent);
 
   return (
     <article>
