a1a6f21317f4310a455ad3c2735020e9d588c33f
Josh Beitler <hello@btlr.xyz>
2025-03-03 20:17:01 -0700

    bookshelf

--- components/nav.js
+++ components/nav.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export const Nav = () => (
   <nav className="flex items-center space-x-2 text-gray-500 text-sm mb-16">
     <Link href="/">@beitler</Link>
-    {/* <span>/</span>
-    <Link href="/thoughts">Thoughts</Link>
     <span>/</span>
     <Link href="/bookshelf">Bookshelf</Link>
+    {/* <span>/</span>
+    <Link href="/thoughts">Thoughts</Link>
     <span>/</span>
     <Link href="/music">Music</Link>
     <span>/</span>
